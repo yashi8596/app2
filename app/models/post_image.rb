01ee81842135b248bc42ae7b0044cbba6a7a3180
yashi8596 <ec2-user@ip-172-31-12-163.ap-northeast-1.compute.internal>
2022-09-05 11:11:21 +0000

    [Finish]app2-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,10 +1,15 @@
 class PostImage < ApplicationRecord
+
   has_one_attached :image
+
   belongs_to :user
+
   has_many :post_comments, dependent: :destroy
-  
+  has_many :favorites, dependent: :destroy
+
 
   def get_image
+
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
@@ -12,4 +17,10 @@ class PostImage < ApplicationRecord
     image
 
   end
+
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
