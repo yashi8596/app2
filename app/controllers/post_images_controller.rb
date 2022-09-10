8480e8122bf6dbb1116ded6bd5be252f5ae67fe4
yashi8596 <deletio.422shinta@gmail.com>
2022-09-10 08:04:34 +0000

    [Finish]app2-22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,44 +1,44 @@
 class PostImagesController < ApplicationController
-  
+
   def new
     @post_image = PostImage.new
   end
 
   def create
-    
+
     @post_image = current_user.post_images.new(post_image_params)
     @post_image.save
-    
-    
+
+
     # @post_image = PostImage.new(post_image_params)
     # @post_image.user_id = current_user.id
     # @post_image.save
     if @post_image.save
-      
+
       redirect_to post_images_path
     else
-      
+
       render :new
     end
-    
+
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
-    
+
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
-    
+
     redirect_to post_images_path
-    
+
   end
 
   def action
